02e99e6985c79951d583761c0da333336ccb8c2f
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-12-03 21:33:03 +0900

    Topコンポーネントの準備完了

--- front/my-app/src/screens/Top.jsx
+++ front/my-app/src/screens/Top.jsx
@@ -1,11 +1,12 @@
-/** ボタンコンポネント */
+/** Topコンポーネント */
 export const Top = () => {
   return (
     <div>
-      トップページです。
-      <div>
+      <p>Topコンポーネント</p>
+     <div>
         <input type="text" />
       </div>
+      <p>Global State : Global State</p>
     </div>
   );
 
