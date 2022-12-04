d7852816b8d16b1b34bb598dfcc1c99277ba861b
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-12-04 18:36:55 +0900

    ComponentBからのstate操作を実装、useContextの確認完了

--- front/my-app/src/screens/ComponentB.jsx
+++ front/my-app/src/screens/ComponentB.jsx
@@ -1,9 +1,21 @@
-/** ボタンコンポネント */
+import { useContext } from "react";
+import { TestContext } from "../providers/TestProvider";
+
+/** ComponentB */
 export const ComponentB = () => {
+
+  /** Global State */
+  const { setGlobalState } = useContext(TestContext);
+
+  /** 入力時の実行関数 */
+  const onChange = (event) => {
+    setGlobalState(event.target.value)
+  }
+
   return (
     <div style={{ marginTop: '100px' }}>
       <h2>ComponentB</h2>
-      <input type="text" />
+      <input type="text" onChange={onChange} />
     </div>
   );
 
