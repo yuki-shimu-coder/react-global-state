fb54d824a30a818c1d36d251698e869ac2ec78d3
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-12-04 15:10:06 +0900

    globalStateの定義

--- front/my-app/src/providers/TestProvider.jsx
+++ front/my-app/src/providers/TestProvider.jsx
@@ -1,12 +1,15 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const TestContext = createContext({});
 
 export const TestProvider = (props) => {
   const { children } = props;
-  const contextName = "テスト";
+
+  /** stateとset関数 */
+  const [globalState, setGlobalState] = useState(null);
   return (
-    <TestContext.Provider value={{ contextName }}>
+    /** stateとset関数を渡すことで、どこからでも更新可能になる。 */
+    <TestContext.Provider value={{ globalState, setGlobalState }}>
       {children}
     </TestContext.Provider>
   );
